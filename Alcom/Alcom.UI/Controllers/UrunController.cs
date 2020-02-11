d33de94ea73168f7ee9fdd22b85a7b250473a6c8
sercanaric <sercanarici@hotmail.com>
2020-02-11 18:44:33 +0300

    urun controller ve view eklendi

---
+++ Alcom/Alcom.UI/Controllers/UrunController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alcom.UI.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
